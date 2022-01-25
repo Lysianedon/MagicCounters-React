19c6dd99f71b3081d8e61a4d0e302293a38e7462
lysiandon <don.lysiane@gmail.com>
2022-01-25 16:47:54 +0100

    bonus 1 : counter counting from 0 to ++

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -8,26 +8,32 @@ class Counter extends React.Component {
     
         this.state = {
           count : 0,
-          //   decrement : this.state.count - 1
         };
     }
     
     render() {
         
-        
-        const obj = {...this.state, increment: this.state.count + 1, decrement: this.state.count - 1};
+        const obj = {...this.state, increment: this.state.count + 1, substract: this.state.count - 1};
+
+
         return (
 
             <div>
                 <h2 style={{marginLeft: 53}}>{this.state.count}</h2>
-
+  
                 <Button content=" + " bgColor = "#d81832" onClick={() => {
                     this.setState({count : obj.increment})
                 }}/>
 
-                <Button content=" - " bgColor = "#90EF90" onClick={() => {
-                this.setState({ count : obj.decrement })
-                }}/>
+                {
+                    obj.count >= 1 ? ( <Button content=" - " bgColor = "#90EF90" onClick={() => {
+                        this.setState({ count : obj.substract })
+                        }}/>) :  ( <Button content=" - " bgColor = "#90EF90" onClick={() => {
+                            this.setState({ count : obj.count})
+                            }}/>)
+                }
+
+
             </div>
         )
     }
