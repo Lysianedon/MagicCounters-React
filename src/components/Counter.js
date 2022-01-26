0049816231daecc3b2784af1f3caf78787415daf
lysiandon <don.lysiane@gmail.com>
2022-01-26 14:29:18 +0100

    moving the state to parent

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,38 +1,46 @@
 import React from "react";
 import Button from "./Buttons";
+import "../styles/counter.css"
+
 
 class Counter extends React.Component {
 
-    constructor() {
-        super();
+    // constructor() {
+    //     super();
     
-        this.state = {
-          count : 0,
-        };
-    }
+    //     this.state = {
+    //       count : 0,
+    //     };
+    // }
     
     render() {
         
-        const obj = {...this.state, increment: this.state.count + 1, substract: this.state.count - 1};
-
+        // const obj = {...this.state, increment: this.state.count + 1, substract: this.state.count - 1};
 
         return (
 
-            <div>
-                <h2 style={{marginLeft: 53}}>{this.state.count}</h2>
-  
-                <Button content=" + " bgColor = "#d81832" onClick={() => {
-                    this.setState({count : obj.increment})
-                }}/>
+            <div className="div">
+
+                {/* Making a condition so that the counter can't exceed 100 */}
 
                 {
-                    obj.count >= 1 ? ( <Button content=" - " bgColor = "#90EF90" onClick={() => {
-                        this.setState({ count : obj.substract })
-                        }}/>) :  ( <Button content=" - " bgColor = "#90EF90" onClick={() => {
-                            this.setState({ count : obj.count})
-                            }}/>)
+                    this.props.count < 100 ? (
+                        <Button content=" + " bgColor = "#90EF90" onClick={this.props.increment} />
+                    ) : (
+
+                        <Button content=" + " bgColor = "#90EF90" onClick={() => {
+                            this.setState({count : this.props.count})
+                        }} />
+                    )
                 }
 
+                <h2>{this.props.count}</h2>
+
+                {/* Making a condition so that the counter can't go below 0 */}
+
+                {
+                    this.props.count >= 1 ? ( <Button content=" - " bgColor = "#d81832" onClick={this.props.substract}/>) :  ( <Button content=" - " bgColor = "#d81832" onClick={this.props.count}/>)
+                }
 
             </div>
         )
